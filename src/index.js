cbc19ecd392ffc2b3259feedb40f2648f9ee82a0
Bjørn Westh <bjwe@itu.dk>
2020-09-29 18:11:53 +0200

    Login sorta works. Need to add fail response db => backend & backend => client

--- src/index.js
+++ src/index.js
@@ -7,48 +7,45 @@
 
 // npm install pg
 // npm install express
+// npm install crypto <--- ??? built in module?
+// npm install express-session
 
 
 // HOW TO RUN!!!
 // Open terminal => cd to ./src => run; node index.js
 
 
-var pg = require('pg');
+const pg = require('pg')
+// const crypto = require('crypto')
+// const keys = crypto.generateKeyPair()
+// console.log(keys)
 
-
-
-const staticQuery = {
-  time: 'SELECT NOW() AS "result"',
-}
-
-function getUser (email) {
-  const query = "SELECT * FROM users WHERE email = '"+email+"'"
-  console.log("Query = "+query)
-  return query
-}
 
 function validateUser (email) {
-  let returned = ''
+  // console.log(email)
+  let data = {}
   var conString = "postgres://qkmmfvxz:YvJJxSK6Q-f2tOR9ErdoBj8CsxREtxAl@rogue.db.elephantsql.com:5432/qkmmfvxz" //Can be found in the Details page
   var client = new pg.Client(conString)
   client.connect(function(err) {
     if(err) {
       return console.error('could not connect to postgres', err)
     }
-    client.query(getUser(email), function(err, result) {
+      client.query('select * from users where email = $1::text', [email], function(err, result) {
       if(err) {
         return console.error('error running query', err)
       }
 
-      // console.log(result.rows[0])
-      returned = result.rows[0]
-      console.log("returned ="+returned)
+      data = result.rows[0]
+      // console.log(data)
       client.end()
-
     })
   })
-  return returned
+  console.log(data)
+  return data
 }
+
+
+
 
 
 
@@ -56,33 +53,46 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  // Pass to next layer of middleware
+  next()
+})
 
 app.get('/', (req, res) => {
   console.log("Yirp")
   res.send('Hello World!')
 })
 
-app.get('/test', (req, res) => {
-  res.send('Test')
-})
-
-app.get('/login', (req, res) => {
+app.post('/login', (req, res) => {
   console.log("Someone tried to log in")
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-  console.log("Query= "+req.query.toString())
-  console.log("Search= "+req.search)
-  // console.log("req.query = "+req.query)
-  // console.log("Search ="+req.search)
-  //search: "?email=mette%40climate.dk&password=hestepige"
+  var conString = "postgres://qkmmfvxz:YvJJxSK6Q-f2tOR9ErdoBj8CsxREtxAl@rogue.db.elephantsql.com:5432/qkmmfvxz" //Can be found in the Details page
+  var client = new pg.Client(conString)
+  client.connect(function(err) {
+    if(err) {return console.error('could not connect to postgres', err)}
 
-  // console.log("email = "+req.query.email)
+    client.query('select * from users where email = $1::text', [req.query.email], function(err, result) {
 
-  const data = validateUser("mette@climate.dk")
-  console.log("data = "+data.toString())
- 
-  // (req.query.password === password) ? res.send({name: name}) : res.send({name: "No no no!"})
-  res.send({name: "Test",data: req})
+    if(err) {return console.error('error running query', err)}
+
+    if(req.query.password === result.rows[0].password) {
+      res.send(result.rows[0])
+    } else {res.end()}
+
+
+    
+
+    client.end()
+  })
+})
 })
 
 
